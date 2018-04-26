8f19345ac1e3bb525bdbbe764788fa7904cbaa6d
snopeks <creativesnopeks@gmail.com>
2018-04-26 16:33:07 -0700

    add passportjs

--- server.js
+++ server.js
@@ -3,10 +3,38 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
-    passport = require('passport').
-    localStrategy = require('passport-local').Strategy
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     db = require('./models')
 
+// configure body parser for req.body data
+app.use(bodyParser.urlencoded({ extended: true, }));
+
+//config auth middleware
+app.use(cookieParser());
+app.use(session({
+  secret: 'supersecretkey', // change this!
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+passport.use(new LocalStrategy(db.User.authenticate()));
+passport.serializeUser(db.User.serializeUser());
+passport.deserializeUser(db.User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  // //Remove caching
+  // res.setHeader('Cache-Control', 'no-cache');
+  // next();
+});
 
 app.get('/api/inventory', function(req, res){
   db.Item.find({})
@@ -43,6 +71,23 @@ app.get('/api/users', function(req, res){
   })
 
 })
+//Signup Route and auth user at the same time
+app.post('/signup', function (req, res) {
+ console.log(req.body.username, req.body.password);
+ db.User.register(new db.User({ username: req.body.username }), req.body.password,
+    function (err, newUser) {
+      if(err){
+        console.log(err);
+        res.status(404).send(err)
+      }
+      passport.authenticate('local')(req, res, function() {
+        console.log(newUser)
+        res.send(newUser);
+      });
+    })
+});
+
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("server started!!");
